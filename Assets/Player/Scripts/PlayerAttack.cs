b82db297623d0c6a4e2d001067ecf9fef233e4f2
JamesBeck04 <bi24cx@student.sunderland.ac.uk>
2023-02-06 16:15:02 +0000

    Week 2 update

---
+++ Assets/Player/Scripts/PlayerAttack.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField]
+    Transform cameraTransform;
+    float range = 100f;
+
+    [SerializeField]
+    float rawDamage = 10f;
+
+    void Update()
+    {
+        FireWeapon();
+    }
+
+    void FireWeapon()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+           
+            Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray, out raycastHit, range))
+            {
+         
+                if (raycastHit.transform != null)
+                {
+                    raycastHit.collider.SendMessageUpwards("Hit", rawDamage, SendMessageOptions.DontRequireReceiver);
+
+                }
+            }
+            else
+            {
+                Debug.Log("NO RAYCAST");
+            }
+        }
+    }
+}
